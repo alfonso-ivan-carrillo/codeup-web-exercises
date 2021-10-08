70939e1b8701b6d382c9c3b81492792bae96ce8c
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2021-10-08 10:50:15 -0500

    Completed the js with html lec

---
+++ js/js-with-html-lec.js
@@ -0,0 +1,15 @@
+// "use strict";
+
+console.log("Hello, I'm in my own file now.");
+
+var returnOfAlert = alert("TGIF! Yeah!");
+console.log(returnOfAlert);
+
+var returnOfConfirm = confirm("Got any plans for the weekend?");
+console.log(returnOfConfirm);
+
+var returnOfPrompt = prompt("What is your favorite rock band?");
+alert(returnOfPrompt + "? Wrong! the only band is Pearl Jam!");
+
+var favoriteBeer = prompt("What is your favorite beer?");
+var leastFavoriteBeer = prompt("What is your least favorite beer?");
