6499f4aac0a93be076b88395f9de6af499b488ba
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2021-11-19 15:47:01 -0700

    move even exercises

--- js/jquery-lesson.js
+++ js/jquery-lesson.js
@@ -1,18 +1,44 @@
 "use strict"
 
 $(document).ready(function(){
-    alert("The document is ready!");
-});
+    // alert("The document is ready!");
 
-// var title = $('#albumDiv').html();
+    // var title = $('#albumDiv').html();
 // alert(title);
+//alert($('#albumDiv').html());
 //
 // var list = $('.list').css("border", "1px red solid");
+//
+//     var li = $("li").css("font-size", "20px");
+//
+//     var highLights = $("h1, p, li").css("background-color", "yellow");
+//     console.log(highLights);
+//
+//     var headerOne = $("h1").html()
+//     alert(headerOne);
+$('#pjHeader').click(function(){
+    $(this).css('background-color', 'blue');
+});
 
-var li = $("li").css("font-size", "20px");
+$('p').dblclick(function(){
+    $(this).css('font-size', '18px');
+});
 
-var hightLights = $("h1, p, li").css("background-color", "yellow");
-console.log(hightLights);
+$('li').hover(
+    function(){
+        $(this).css('background-color', 'red');
+    }, function(){
+        $(this).css('background-color', 'white');
+    }
+);
 
-var headerOne = $("h1").html()
-    alert(headerOne);
+
+
+
+
+
+
+
+});
+
+
