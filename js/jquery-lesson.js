7f517d83ca1ad07a9fa70735b66ae87414f1860f
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2021-11-19 10:24:09 -0700

    selector exercises

--- js/jquery-lesson.js
+++ js/jquery-lesson.js
@@ -3,3 +3,16 @@
 $(document).ready(function(){
     alert("The document is ready!");
 });
+
+// var title = $('#albumDiv').html();
+// alert(title);
+//
+// var list = $('.list').css("border", "1px red solid");
+
+var li = $("li").css("font-size", "20px");
+
+var hightLights = $("h1, p, li").css("background-color", "yellow");
+console.log(hightLights);
+
+var headerOne = $("h1").html()
+    alert(headerOne);
